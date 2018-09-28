ddf891d42b7877e8e46fd975b6894c9bf920d374
vagrant <vagrant@homestead>
2018-09-28 01:56:00 +0000

    First Commit. Local server has been provisioned. Beginning back end. Models have been created

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Example",
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/assets/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <Example></Example>
+
+  <h1>Sample Task App on Laravel Homestead</h1>
+  <h4>Laravel & Vue.js</h4>
+  </div>
+</template>
+
+<script>
+  import Example from '../components/Example';
+  export default{
+    components:{
+      Example
+    }
+  }
+
+</script>
